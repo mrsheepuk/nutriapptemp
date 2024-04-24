2105613a8397cc947ddccdded3eafc2ea9e5046b
Mark Hughes <mark.hughes@appvia.io>
2024-04-24 20:42:25 +0100

    adding drag behavr

--- dummy-nutri/src/App.js
+++ dummy-nutri/src/App.js
@@ -1,27 +1,36 @@
-import logo from './logo.svg';
-import CookieTwoToneIcon from '@mui/icons-material/CookieTwoTone';
-import './App.css';
+import logo from "./logo.svg";
+import CookieTwoToneIcon from "@mui/icons-material/CookieTwoTone";
+import Draggable from "react-draggable";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const[position,setposition]=useState(null)
+
+function positionHandler(e,d){
+setposition({x:d.x,y:d.y})  
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <CookieTwoToneIcon style={{fontSize:'72px'}}/>
-          Learn React
-        </a>
+        <div style={{width:'100px', height:"100px", backgroundColor:"pink" ,position:'absolute' ,top:'10px',left:'10px'}}></div>
+        <span style={{width:'100px', height:"100px", backgroundColor:"green",position:'absolute' ,top:'10px',left:'110px'}}></span>
+        <Draggable position={position} onStop={positionHandler}>
+            <CookieTwoToneIcon style={{ fontSize: "72px" }} />
+          </Draggable>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+/*onStart: DraggableEventHandler,
+
+// Called while dragging.
+onDrag: DraggableEventHandler,
+
+// Called when dragging stops.
+onStop: DraggableEventHandler,
+*/ 
